4313d2cd2344ba2e6824e2e41ed15bf66ed53e85
blakebdavis <blake.davis@credera.com>
2023-06-08 13:49:51 -0500

    Update ProductDto.cs

--- dotnet/GroupProjectApi/Modules/Models/ProductDto.cs
+++ dotnet/GroupProjectApi/Modules/Models/ProductDto.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace GroupProjectApi.Modules.Models
 {
+    // public class Hotsauce
+    // {
+    //     public List<ProductDto> Productds {get; set;}
+    // }
     public class ProductDto
-    {
+
+    {   
         public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
