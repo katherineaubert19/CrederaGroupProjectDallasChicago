bddfc30b74fee4bf802fd8e3e3ecc573d79e24a0
katymeston-wardd <katy.meston.ward@credera.com>
2023-06-09 08:44:41 -0500

    small dto fix

--- dotnet/GroupProjectApi/Modules/Models/ProductDto.cs
+++ dotnet/GroupProjectApi/Modules/Models/ProductDto.cs
@@ -9,7 +9,7 @@ namespace GroupProjectApi.Modules.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public string Image {get; set;}
+        public string Description { get; set; }
     }
 
     public class ProductDetail
